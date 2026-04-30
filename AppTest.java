92ed8c1fca458744941319e7dd667bfe604707ec
Kinhao <106199302+devkinhao@users.noreply.github.com>
2026-04-29 21:19:54 -0300

    Update AppTest.java

--- AppTest.java
+++ AppTest.java
@@ -1,13 +1,20 @@
 public class AppTest {
+
     public static void main(String[] args) {
         ClienteService service = new ClienteService();
 
         service.cadastrar("Teste", "teste@email.com");
 
-        if (service.total() != 1) {
-            throw new RuntimeException("Teste falhou!");
-        }
+        assertEquals(1, service.total(), "Total de clientes deveria ser 1");
 
         System.out.println("Teste passou com sucesso!");
+    }
+
+    private static void assertEquals(int esperado, int atual, String mensagem) {
+        if (esperado != atual) {
+            throw new AssertionError(
+                mensagem + " | esperado: " + esperado + ", atual: " + atual
+            );
+        }
     }
 }
